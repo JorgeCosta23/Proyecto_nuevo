7b0ac2c736350b8b70316a22434991e9c7ca1333
JorgeCosta23 <costasobarzoj@gmail.com>
2022-11-05 01:48:44 -0400

    ACTUALIZADO

--- app/src/main/java/twin/developers/recyclerview/botonesactivity.java
+++ app/src/main/java/twin/developers/recyclerview/botonesactivity.java
@@ -32,7 +32,7 @@ public class botonesactivity extends AppCompatActivity {
 
             }
         });
-        Button cerrarsesion = (Button) findViewById(R.id.idlistalocales);
+        Button cerrarsesion = (Button) findViewById(R.id.idcerrarsesion);
         cerrarsesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
